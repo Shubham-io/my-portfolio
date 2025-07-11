d91b240e90ccba3a31ef4b3685b843a0bd376b2c
Shubham Vishwakarma <no_reply@gmail.com>
2025-07-11 19:21:02 +0530

    update favicon

--- portfolio/src/assets/assets.js
+++ portfolio/src/assets/assets.js
@@ -8,7 +8,7 @@ import git from "./git.png";
 import html from "./html.png";
 import javascript from "./javascript.png";
 import mongodb from "./mongodb.png";
-import profile_pic from "./profile_pic.png";
+import profile_pic1 from "./profile_pic1.png";
 import nodejs from "./nodejs.png";
 import passManager from "./PassManager.png";
 import postman from "./postman.png";
@@ -21,7 +21,7 @@ import tailwind from "./tailwind.png";
 export const assets = {
   clinic,
   grocify,
-  profile_pic,
+  profile_pic1,
   cpp,
   currencyCon,
   expenseTracker,
