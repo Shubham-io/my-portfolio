bd752ec67ebfd80a972fbdf18e89b7223f1f6dcb
Shubham Vishwakarma <no_reply@gmail.com>
2025-07-14 12:37:15 +0530

    update TechStack component

--- portfolio/src/components/TechStack.jsx
+++ portfolio/src/components/TechStack.jsx
@@ -1,65 +1,7 @@
 import React from "react";
-import { assets } from "../assets/assets.js";
+import { assets, MyTechStack } from "../assets/assets.js";
 
 const TechStack = () => {
-  const cardItems = [
-    {
-      id: 1,
-      logo: assets.html,
-      name: "HTML",
-    },
-    {
-      id: 2,
-      logo: assets.css,
-      name: "CSS",
-    },
-    {
-      id: 3,
-      logo: assets.javascript,
-      name: "JavaScript",
-    },
-    {
-      id: 4,
-      logo: assets.react,
-      name: "React",
-    },
-    {
-      id: 5,
-      logo: assets.nodejs,
-      name: "NodeJs",
-    },
-    {
-      id: 6,
-      logo: assets.mongodb,
-      name: "MongoDb",
-    },
-    {
-      id: 7,
-      logo: assets.tailwind,
-      name: "Tailwind CSS",
-    },
-    {
-      id: 8,
-      logo: assets.python,
-      name: "Python",
-    },
-    {
-      id: 9,
-      logo: assets.cpp,
-      name: "C++",
-    },
-    {
-      id: 10,
-      logo: assets.git,
-      name: "Git",
-    },
-    {
-      id: 11,
-      logo: assets.postman,
-      name: "Postman",
-    },
-  ];
-
   return (
     <div
       name="Tech Stack"
@@ -71,13 +13,13 @@ const TechStack = () => {
           I have worked with mentioned tools and technologies.
         </p>
         <div className="grid grid-cols-2 md:grid-cols-5 gap-6 my-4">
-          {cardItems.map(({ id, logo, name }) => (
+          {MyTechStack.map(({ id, logo, name }) => (
             <div
               key={id}
               className="md:w-[125px] md:h-[125px] flex flex-col justify-center items-center border-[2px] cursor-pointer rounded-lg p-1 shadow-lg hover:scale-105 duration-300"
             >
               <img src={logo} alt="logo" className="w-[80px]  " />
-              <span>{name}</span>
+              <span className="text-center text-sm">{name}</span>
             </div>
           ))}
         </div>
