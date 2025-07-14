d046b99b0cee97162f2e8cbb2afea4e2c424e72b
Shubham Vishwakarma <no_reply@gmail.com>
2025-07-14 12:36:37 +0530

    update assets

--- portfolio/src/assets/assets.js
+++ portfolio/src/assets/assets.js
@@ -17,6 +17,9 @@ import python from "./python.png";
 import quizApp from "./QuizApp.png";
 import react from "./react.png";
 import tailwind from "./tailwind.png";
+import canva from "./canva.png";
+import gcp from "./gcp.png";
+import figma from "./figma.png";
 
 export const assets = {
   clinic,
@@ -38,4 +41,80 @@ export const assets = {
   quizApp,
   react,
   tailwind,
+  figma,
+  canva,
+  gcp,
 };
+
+export const MyTechStack = [
+    {
+      id: 1,
+      logo: assets.html,
+      name: "HTML",
+    },
+    {
+      id: 2,
+      logo: assets.css,
+      name: "CSS",
+    },
+    {
+      id: 3,
+      logo: assets.javascript,
+      name: "JavaScript",
+    },
+    {
+      id: 4,
+      logo: assets.react,
+      name: "React",
+    },
+    {
+      id: 5,
+      logo: assets.nodejs,
+      name: "NodeJs",
+    },
+    {
+      id: 6,
+      logo: assets.mongodb,
+      name: "MongoDb",
+    },
+    {
+      id: 7,
+      logo: assets.tailwind,
+      name: "Tailwind CSS",
+    },
+    {
+      id: 8,
+      logo: assets.python,
+      name: "Python",
+    },
+    {
+      id: 9,
+      logo: assets.cpp,
+      name: "C++",
+    },
+    {
+      id: 10,
+      logo: assets.git,
+      name: "Git",
+    },
+    {
+      id: 11,
+      logo: assets.postman,
+      name: "Postman",
+    },
+    {
+      id: 12,
+      logo: assets.figma,
+      name: "Figma",
+    },
+    {
+      id: 13,
+      logo: assets.canva,
+      name: "Canva",
+    },
+    {
+      id: 11,
+      logo: assets.gcp,
+      name: "Google Cloud Platform",
+    },
+  ];
