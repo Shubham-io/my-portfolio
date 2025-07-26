df97c4fcb2ceb1c59476d24bff598bcdee57cc38
Shubham Vishwakarma <no_reply@gmail.com>
2025-07-26 17:57:09 +0530

    add scroll-linked_animation

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,20 +5,40 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import  { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import TechStack from "./components/TechStack";
+import { motion, useScroll } from "motion/react";
 
 const App = () => {
+  const { scrollYProgress } = useScroll();
   return (
     <>
-      <Navbar />
-      <Home />
-      <About />
-      <Portfolio />
-      <TechStack />
-      <Contact />
-      <Footer />
-      <Toaster />
+      <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 4,
+          originX: 0,
+          zIndex: 20,
+        
+          backgroundColor: "#B91C1C",
+        }}
+      />
+
+      <div>
+        <Navbar />
+        <Home />
+
+        <About />
+        <Portfolio />
+        <TechStack />
+        <Contact />
+        <Footer />
+        <Toaster />
+      </div>
     </>
   );
 };
